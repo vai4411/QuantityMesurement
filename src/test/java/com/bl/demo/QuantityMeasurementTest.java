00898378e860d7ac6248bb3a35ea8bd6399981b9
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-05-17 20:28:17 +0530

    ADDED: If One Yard And ThirtySix Inch Then Return True

--- src/test/java/com/bl/demo/QuantityMeasurementTest.java
+++ src/test/java/com/bl/demo/QuantityMeasurementTest.java
@@ -9,12 +9,14 @@ import org.junit.Test;
 
 public class QuantityMeasurementTest {
 
+    //TC-1.1
     @Test
     public void givenFeetAndFeet_WhenZero_ShouldReturnTrue() {
         QuantityMeasurement quantity = new QuantityMeasurement();
         Assert.assertTrue(quantity.equals(new Feet(0),new Feet(0)));
     }
 
+    //TC-1.2
     @Test
     public void givenFeetAndFeet_WhenOneIsNull_ShouldReturnFalse() {
         Feet feet = null;
@@ -22,6 +24,7 @@ public class QuantityMeasurementTest {
         Assert.assertFalse(quantity.equals(feet,new Feet(0)));
     }
 
+    //TC-1.3
     @Test
     public void givenFeetAndFeet_WhenSameType_ShouldReturnTrue() {
         Feet one = new Feet(0);
@@ -30,6 +33,7 @@ public class QuantityMeasurementTest {
         Assert.assertTrue(quantity.equals(one,two));
     }
 
+    //TC-1.4
     @Test
     public void givenFeetAndFeet_WhenSameObject_ShouldReturnTrue() {
         Feet feet = new Feet(0);
@@ -37,6 +41,7 @@ public class QuantityMeasurementTest {
         Assert.assertTrue(quantity.equals(feet,feet));
     }
 
+    //TC-1.5
     @Test
     public void givenFeetAndFeet_WhenSameLength_ShouldReturnTrue() {
         Feet one = new Feet(10);
@@ -51,6 +56,7 @@ public class QuantityMeasurementTest {
         Assert.assertFalse(quantity.equals(new Feet(0),new Feet(10)));
     }
 
+    //TC-1.6
     @Test
     public void givenInchAndInch_WhenOneIsNull_ShouldReturnFalse() {
         Inch inch = null;
@@ -58,6 +64,7 @@ public class QuantityMeasurementTest {
         Assert.assertFalse(quantity.equals(inch,new Inch(0)));
     }
 
+    //TC-1.7
     @Test
     public void givenInchAndInch_WhenSameType_ShouldReturnTrue() {
         Inch one = new Inch(0);
@@ -66,6 +73,7 @@ public class QuantityMeasurementTest {
         Assert.assertTrue(quantity.equals(one,two));
     }
 
+    //TC-1.8
     @Test
     public void givenInchAndInch_WhenSameObject_ShouldReturnTrue() {
         Inch inch = new Inch(0);
@@ -73,6 +81,7 @@ public class QuantityMeasurementTest {
         Assert.assertTrue(quantity.equals(inch,inch));
     }
 
+    //TC-1.9
     @Test
     public void givenInchAndInch_WhenSameLength_ShouldReturnTrue() {
         Inch one = new Inch(10);
@@ -89,24 +98,28 @@ public class QuantityMeasurementTest {
         Assert.assertFalse(quantity.equals(one,two));
     }
 
+    //TC-1.10
     @Test
     public void givenFeetAndInch_WhenZero_ShouldReturnTrue() {
         QuantityMeasurement quantity = new QuantityMeasurement();
         Assert.assertTrue(quantity.equals(new Feet(0),new Inch(0)));
     }
 
+    //TC-1.11
     @Test
     public void givenFeetAndInch_WhenOne_ShouldReturnFalse() {
         QuantityMeasurement quantity = new QuantityMeasurement();
         Assert.assertFalse(quantity.equals(new Feet(1),new Inch(1)));
     }
 
+    //TC-1.12
     @Test
     public void givenFeetAndInch_WhenOneFeetAndTwelveInch_ShouldReturnTrue() {
         QuantityMeasurement quantity = new QuantityMeasurement();
         Assert.assertTrue(quantity.equals(new Feet(1),new Inch(12)));
     }
 
+    //TC-1.13
     @Test
     public void givenInchAndFeet_WhenTwelveInchAndOneFeet_ShouldReturnTrue() {
         QuantityMeasurement quantity = new QuantityMeasurement();
@@ -123,21 +136,31 @@ public class QuantityMeasurementTest {
         }
     }
 
+    //TC-1.14
     @Test
     public void givenFeetAndYard_WhenThreeFeetAndOneYard_ShouldReturnTrue() {
         QuantityMeasurement quantity = new QuantityMeasurement();
         Assert.assertTrue(quantity.equals(new Feet(3),new Yard(1)));
     }
 
+    //TC-1.15
     @Test
     public void givenFeetAndYard_WhenOne_ShouldReturnFalse() {
         QuantityMeasurement quantity = new QuantityMeasurement();
         Assert.assertFalse(quantity.equals(new Feet(1),new Yard(1)));
     }
 
+    //TC-1.16
     @Test
     public void givenInchAndYard_WhenOne_ShouldReturnFalse() {
         QuantityMeasurement quantity = new QuantityMeasurement();
         Assert.assertFalse(quantity.equals(new Inch(1),new Yard(1)));
+    }
+
+    //TC-1.17
+    @Test
+    public void givenYardAndInch_WhenOneYardAndThirtySixInch_ShouldReturnTrue() {
+        QuantityMeasurement quantity = new QuantityMeasurement();
+        Assert.assertTrue(quantity.equals(new Yard(1),new Inch(36)));
     }
 }
